314c0b427de2aa06babae4221e7262e8845077ae
Smael Nicolas <nico.smael@gmail.com>
2022-03-29 09:22:38 -0300

    footer

--- src/Pages/Home/Sections/Footer/Contact/Contact.jsx
+++ src/Pages/Home/Sections/Footer/Contact/Contact.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import "./Contact.css";
+
+import Logo from "../../../../../Assets/Logo.svg";
+import imgIg from "../../../../../Assets/Footer/Instagram.svg";
+import imgFb from "../../../../../Assets/Footer/Facebook.svg";
+import imgTw from "../../../../../Assets/Footer/Twitter.svg";
+import imgLnk from "../../../../../Assets/Footer/Linkedin.svg";
+import imgWtsp from "../../../../../Assets/Footer/Whatsapp.svg";
 
 function Contact() {
-	return <div>Contact</div>;
+	return (
+		<div className='contact' id='contact'>
+			<div className='contact--content'>
+				<div className='contact--content--left ai-center'>
+					<div className='contact--top--about--img'>
+						<img src={Logo} alt='logo' />
+					</div>
+					<div className='contact--top--about--text'>
+						Comunidad comprometida con el Medio Ambiente. Creando
+						soluciones para combatir la Contaminación Cuidando
+						Nuestro Planeta
+					</div>
+				</div>
+				<div className='contact--content--right fd-column jc-start ai-center'>
+					<div className='contact--content--title ta-center'>
+						Siguenos en
+					</div>
+					<div className='contact--top--socials--icons'>
+						<img
+							src={imgIg}
+							alt='logo ig'
+							className='contact--top--socials--icons--link'
+						/>
+						<img
+							src={imgFb}
+							alt=' logo fb'
+							className='contact--top--socials--icons--link'
+						/>
+						<img
+							src={imgTw}
+							alt=''
+							className='contact--top--socials--icons--link'
+						/>
+						<img
+							src={imgLnk}
+							alt=''
+							className='contact--top--socials--icons--link'
+						/>
+					</div>
+				</div>
+			</div>
+			<div className='contact--content'>
+				<div className='contact--content--left fd-column jc-end '>
+					<div className='contact--bottom--direct--phone'>
+						<div className='contact--content--title'>Contacto</div>
+						<div className='contact--bottom--direct--phone--number'>
+							+123 456 789 0
+						</div>
+					</div>
+					<div className='contact--bottom--direct--email'>
+						<div className='contact--content--title'>Email</div>
+						<div className='contact--bottom--direct--email-direction'>
+							shifttrek@gmail.com
+						</div>
+					</div>
+				</div>
+				<div className='contact--content--right fd-row jc-center ai-end'>
+					<div className='contact--bottom--wtsp--content'>
+						<div className='contact--content--title ta-end'>
+							¿Deseas comunicarte con nosotros?
+						</div>
+						<div className='contact--bottom--wtsp--content--text'>
+							Puedes enviarnos un whatsapp aqui
+						</div>
+					</div>
+					<img
+						src={imgWtsp}
+						alt=''
+						className='contact--top--socials--icons--link'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
